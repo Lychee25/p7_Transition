8a4d87552542a5173157af6abce123c6339a8b78
Lychee25 <129222442+Lychee25@users.noreply.github.com>
2023-03-29 21:05:28 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function button(x) {
+    window.location.href = "wakeup.html"
+}
+
+function button2(x) {
+    window.location.href = "wakeupcon.html"
+}
+
+function notie(x) {
+    window.location.href = "notie.html";
+}
+
+function tiehelp(x) {
+    window.location.href = "tiehelp.html";
+}
+function old(x) {
+    window.location.href = "walkingold.html";
+}
+function oldcon(x) {
+    window.location.href = "oldfri_notie.html";
+}
+function regifri(x) {
+    window.location.href = "regifri.html";
+}
+function regifricon(x) {
+    window.location.href = "regifri_notie.html";
+}
+
+function run(x) {
+    window.location.href = "run.html";
+}
+function runcon(x) {
+    window.location.href = "run_notie.html";
+}
+function walk(x) {
+    window.location.href = "walk.html";
+}
+function walkcon(x) {
+    window.location.href = "walk_notie.html";
+}
+function regi(x) {
+    window.location.href = "class.html";
+}
+function regicon(x) {
+    window.location.href = "class_notie.html";
+}
+
