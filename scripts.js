520109a2c513f89c895287d5283617245a884508
Lychee25 <129222442+Lychee25@users.noreply.github.com>
2023-03-31 18:44:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,32 +16,18 @@ function tiehelp(x) {
 function old(x) {
     window.location.href = "walkingold.html";
 }
-function oldcon(x) {
-    window.location.href = "oldfri_notie.html";
-}
+
 function regifri(x) {
     window.location.href = "regifri.html";
-}
-function regifricon(x) {
-    window.location.href = "regifri_notie.html";
 }
 
 function run(x) {
     window.location.href = "run.html";
 }
-function runcon(x) {
-    window.location.href = "run_notie.html";
-}
 function walk(x) {
     window.location.href = "walk.html";
 }
-function walkcon(x) {
-    window.location.href = "walk_notie.html";
-}
 function regi(x) {
     window.location.href = "class.html";
-}
-function regicon(x) {
-    window.location.href = "class_notie.html";
 }
 
